7cb2b772d75bd1dc82197c22242e7cb5d2dd6715
Omegasz <omegasz77@gmail.com>
2014-03-07 16:35:40 +0100

    1.5.4
    Modification de liens

--- FreeBedProject/recherche.php
+++ FreeBedProject/recherche.php
@@ -2,13 +2,15 @@
 session_start();
 include("php/connexion.php");
 if(isset($_POST["email"])){
-	$email =$_POST ["email"];
+	$email =$_POST["email"];
 	$password = $_POST["password"];
-	$query = "SELECT * FROM User WHERE email = '".$email."' AND password = '".$password."' ; ";
+	$query = "SELECT *, u.id as id_u FROM user u JOIN type t ON t.id = u.id_type WHERE email = '".$email."' AND password = '".$password."' ; ";
 	$result = mysqli_query($connexion, $query);
+
 	if(mysqli_num_rows($result) != 0){
 		$user = mysqli_fetch_array($result);
-		$_SESSION["userId"] = $user['id'];
+		$_SESSION["userId"] = $user['id_u'];
+		$_SESSION["typeId"] = $user["id_type"];
 	}else{
 		function myFunction()
 		{
@@ -16,11 +18,13 @@ if(isset($_POST["email"])){
 		}
 	}
 }
+
+
 if(isset($_SESSION["userId"])){
-	$query = "SELECT * FROM User WHERE id=".$_SESSION["userId"]."";
+	$query = "SELECT * FROM user WHERE id=".$_SESSION["userId"]."";
 	$result = mysqli_query($connexion, $query);
 	$user = mysqli_fetch_array($result);
 }
 
-include('recherche.html');
+include('index.html');
 ?>
